72caabc0fa49acbb6835cce7b282ecd622ee69e5
CristianP50 <cristo>
2022-09-05 12:11:39 -0500

    se organiza tokje

--- src/services/servicioGenerarToken.js
+++ src/services/servicioGenerarToken.js
@@ -13,8 +13,28 @@ const DATO3='client_secret=455cb4e4a7664f49ac968d468beb29a1'
 
 const PETICION= {
     method :"POST",
-    headers : {},
+    headers : {
+        'content-type': "application/x-www-form-urlencoded"
+
+    },
     body:DATO1+"&"+DATO2+"&"+DATO3
 }
 
 //FETCH
+
+fetch(URI,PETICION)
+.then(function(respuesta){
+    return respuesta.json()
+})
+.then(function(respuesta){
+    console.log(respuesta)
+})
+.catch(function(respuesta){
+    console.log(respuesta)
+})
+
+async function obtenerToken(){
+    let respuesta =await fetch(URI,PETICION)
+    return await respuesta.json()
+}
+console.log(await obtenerToken())
